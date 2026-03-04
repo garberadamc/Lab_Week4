a9ae925aa586ff89a4f2e706f42d343cbfc23f1b
Adam Garber <garberadamc@gmail.com>
2026-03-04 09:15:39 -0800

    update S26

---
+++ backup_install.R
@@ -0,0 +1,23 @@
+
+
+# install
+
+install.packages("psych")
+install.packages("tidyverse")
+install.packages("janitor")
+install.packages("here")
+install.packages("sjPlot")
+install.packages("GGally")
+install.packages("corrplot")
+install.packages("reshape2")
+
+# load
+
+library(psych)
+library(tidyverse)
+library(janitor)
+library(here)
+library(sjPlot)
+library(GGally)
+library(corrplot)
+library(reshape2)
